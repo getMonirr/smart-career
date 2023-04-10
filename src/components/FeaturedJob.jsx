170735a3b5ef66058803cf074cf1df024a665c95
Monir Hoshen <getmonirr@gmail.com>
2023-04-10 15:30:15 +0600

    featured section responsive done

--- src/components/FeaturedJob.jsx
+++ src/components/FeaturedJob.jsx
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom";
 const FeaturedJob = ({ job }) => {
   const { logo, title, name, type, timeType, salary, address, id } = job;
   return (
-    <div className="p-10 border rounded-lg border-d6">
+    <div className="p-5 lg:p-10 border rounded-lg border-d6">
       <div className="">
         <img className="mb-8 object-cover origin-center" src={logo} alt={name} />
       </div>
@@ -22,7 +22,7 @@ const FeaturedJob = ({ job }) => {
         <button className="jobs-btn mr-4">{type}</button>
         <button className="jobs-btn">{timeType}</button>
       </div>
-      <div className="flex items-center space-x-6">
+      <div className="md:flex items-center md:space-x-6 space-y-2 md:space-y-0 mt-8 md:mt-0">
         <p className="inline-flex items-center gap-2">
           <img src={locationIcon} alt={locationIcon} />
           <span className="text-base font-semibold text-d3">{address}</span>
