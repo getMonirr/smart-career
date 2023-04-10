df07cae560897f8356e4d8f9c074791d7b987f93
Monir Hoshen <getmonirr@gmail.com>
2023-04-11 01:30:59 +0600

    filter job done

--- src/components/AppliedJobs.jsx
+++ src/components/AppliedJobs.jsx
@@ -1,17 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DetailsCommonCompo from "./DetailsCommonCompo";
 import { useLoaderData } from "react-router-dom";
 import AppliedJob from "./AppliedJob";
 
 const AppliedJobs = () => {
   const appliedJobs = useLoaderData();
-  const [jobCart, setJobCart] = useState(appliedJobs);
+  const [filteredJob, setFilteredJob] = useState(appliedJobs);
+
+  // handle filter
+  const handleFilter = (e) => {
+    const filterOption = e.target.value;
+    const filteredAppliedJob = appliedJobs.filter((ajb) => {
+      if (filterOption.toLocaleLowerCase() !== "all") {
+        return (
+          ajb.type.toLocaleLowerCase() === filterOption.toLocaleLowerCase()
+        );
+      }
+      return ajb;
+    });
+    setFilteredJob(filteredAppliedJob);
+  };
+
   return (
     <>
       <DetailsCommonCompo>Applied Jobs</DetailsCommonCompo>
       <div className="sm-container my-32">
+        <div className="text-end mb-8">
+          <select
+            onChange={handleFilter}
+            className="select select-bordered w-32 max-w-xs rounded-lg"
+          >
+            <option disabled value>
+              Filter By
+            </option>
+            <option>All</option>
+            <option>Remote</option>
+            <option>Onsite</option>
+          </select>
+        </div>
         <div>
-          {jobCart.map((jb) => (
+          {filteredJob.map((jb) => (
             <AppliedJob key={jb.id} appliedJob={jb} />
           ))}
         </div>
