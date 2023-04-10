89a46dfb9803bbf0cc22acc5a2cbc44af3c3e367
Monir Hoshen <getmonirr@gmail.com>
2023-04-10 13:50:27 +0600

    category resposive done

--- src/components/JobCategories.jsx
+++ src/components/JobCategories.jsx
@@ -9,13 +9,15 @@ const JobCategories = () => {
   return (
     <div className="sm-container my-32">
       <div className="text-center">
-        <h3 className="text-5xl font-extrabold mb-4">Job Category List</h3>
-        <p>
+        <h3 className="text-3xl md:text-5xl font-extrabold mb-4 text-d1">
+          Job Category List
+        </h3>
+        <p className="text-d3">
           Explore thousands of job opportunities with all the information you
           need. Its your future
         </p>
       </div>
-      <div className="grid grid-cols-4 gap-6 mt-8 mb-32">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-8 mb-32">
         {categories.map((c) => (
           <JobCategory key={c.id} category={c} />
         ))}
